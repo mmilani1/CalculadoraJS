8c793e506e5b039b8abd040572548e9a0f1d0044
Matheus Milani <milani.matheus@codus.com.br>
2018-12-04 17:26:50 -0200

    Correção de bug no botao igual. MathError em alguns casos não definidos

--- calculatorScript.js
+++ calculatorScript.js
@@ -1,11 +1,17 @@
-var displayValue = "";
-var fisrtNumber = 0;
-var secondNumber = null;
-var operation = null;
-var register = null;
-var equalPressed = 0;
-var display = document.getElementById('display');
+var displayValue = "";                                            // Variável para impressão no display
+var fisrtNumber = 0;                                              // Primeiro termo do cálculo
+var secondNumber = null;                                          // Segundo termo do cálculo
+var operation = null;                                             // Ultimo operador pressionado
+var register = null;                                              // Variável acumuladora de resultados
+var equalPressed = 0;                                             // Controle de estado para o botão de igualdade
+var display = document.getElementById('display');                 // Elemento HTML que recebe os valores para o display
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+ / Atibuição das funções para cada evento de 'onclick' dos botões 
+ / do teclado da calculadora.
+*/
 keys.onclick = function(event){
   button = event.target;
   if(button.className.indexOf("number") > -1){
@@ -32,6 +38,11 @@ document.getElementById('clear').onclick = function(){
   clear();
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+ / Função de impressão no display. Corrige problemas de formato da saída
+*/
 function printToDisplay(output) {
   var outputLength = output.length;
   output = parseFloat(output);
@@ -47,9 +58,19 @@ function printToDisplay(output) {
     output = "Math Error";
   }
   display.innerHTML = output;
+  console.log("n1 = " + fisrtNumber, "n2 = " + secondNumber, "reg = " + register);
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+ / Função que armazena os inputs numéricos do usuário e mostra as
+ / entradas no display.
+*/
 function getInput(value){
+  if(equalPressed != 0){
+    clear();
+  }
   displayValue = displayValue + value;
   if (operation == null){
     fisrtNumber = displayValue;
@@ -60,17 +81,31 @@ function getInput(value){
   calculateToRegister();
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+ / Função de 'reset', re-inicializando variáveis necessárias e o diplay.
+*/
 function clear() {
   operation = null;
   displayValue = "0";
+  equalPressed = 0;
   fisrtNumber = 0;
   secondNumber = null;
   register = null;
   printToDisplay(displayValue.toString());
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+ / Função que recebe um identificador de operação quando ativada, armazenando
+ / para cálculos sequenciais.
+ /
+ / Re-inicializa o display para a entrada de um novo termo para calcular.
+*/
 function operationPressed (operationNumber) {
-  equalPressed = false;
+  equalPressed = 0;
   if(register != null){
     fisrtNumber = register;
   }
@@ -84,28 +119,49 @@ function operationPressed (operationNumber) {
   }
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+ / Função para o botão de igualdade.
+ / Calculos sequenciais são possiveis.
+*/
 function equal (){
   fisrtNumber = register;
   if (equalPressed != 0){
     calculateToRegister();
   }
   var finalResult = register;
+  displayValue = "";
+  if (finalResult == null){
+    printToDisplay("");
+  } else {
   printToDisplay(finalResult.toString());
+  }
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+ / Executa as operações e guarda no acumulador, baseado no último operador 
+ / pressionado.
+*/
 function calculateToRegister() {
   switch (operation) {
     case "+":
-      register = parseFloat(fisrtNumber) + parseFloat(secondNumber);
+      register = parseFloat(fisrtNumber) +
+        parseFloat(secondNumber);
       break;
     case "-":
-      register = (parseFloat(fisrtNumber) - parseFloat(secondNumber)).toPrecision(15) * 1;
+      register =(parseFloat(fisrtNumber) -
+        parseFloat(secondNumber)).toPrecision(15) * 1;
       break;
     case "x":
-      register = parseFloat(fisrtNumber) * parseFloat(secondNumber);
+      register = parseFloat(fisrtNumber) *
+        parseFloat(secondNumber);
       break;
     case "/":
-      register = parseFloat(fisrtNumber) / parseFloat(secondNumber);
+      register = parseFloat(fisrtNumber) /
+        parseFloat(secondNumber);
       break;  
     default:
       break;
