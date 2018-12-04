991f9fc6c571bf163b854f35b384150c63bf5c93
Matheus Milani <milani.matheus@codus.com.br>
2018-12-04 15:24:58 -0200

    Ajustes em classes e IDs no HTML/CSS. Totalmente funcional e display ajustado

--- calculatorScript.js
+++ calculatorScript.js
@@ -1,5 +1,5 @@
 var displayValue = "";
-var fisrtNumber = null;
+var fisrtNumber = 0;
 var secondNumber = null;
 var operation = null;
 var register = null;
@@ -10,55 +10,65 @@ document.getElementById('clear').onclick = function(){
     clear();
 }
 document.getElementById('zero').onclick = function(){
-    pushToDisplay('0');
+   getInput('0');
 }
 document.getElementById('one').onclick = function(){
-    pushToDisplay('1');
+   getInput('1');
 }
 document.getElementById('two').onclick = function(){
-    pushToDisplay('2');
+   getInput('2');
 }
 document.getElementById('three').onclick = function(){
-    pushToDisplay('3');
+   getInput('3');
 }
 document.getElementById('four').onclick = function(){
-    pushToDisplay('4');
+   getInput('4');
 }
 document.getElementById('five').onclick = function(){
-    pushToDisplay('5');
+   getInput('5');
 }
 document.getElementById('six').onclick = function(){
-    pushToDisplay('6');
+   getInput('6');
 }
 document.getElementById('seven').onclick = function(){
-    pushToDisplay('7');
+   getInput('7');
 }
 document.getElementById('eight').onclick = function(){
-    pushToDisplay('8');
+   getInput('8');
 }
 document.getElementById('nine').onclick = function(){
-    pushToDisplay('9');
+   getInput('9');
 }
 document.getElementById('nine').onclick = function(){
-    pushToDisplay('9');
+   getInput('9');
 }
 document.getElementById('plus').onclick = function(){
-  operationPressed(0);
+  operationPressed("+");
 }
 document.getElementById('minus').onclick = function(){
-  operationPressed(1);
+  operationPressed("-");
 }
 document.getElementById('times').onclick = function(){
-  operationPressed(2);
+  operationPressed("x");
 }
 document.getElementById('division').onclick = function(){
-  operationPressed(3);
+  operationPressed("/");
 }
+
+/*keys.onclick = function(event){
+  button = event.target;
+  if(button.className.indexOf("number") > -1){
+    getInput(button.textContent);
+  } else if (button.className.indexOf("operator") > -1) {
+    operationPressed(button.textContent);
+  }
+}*/
+
 document.getElementById('dot').onclick = function(){
   if(displayValue == ""){
-    pushToDisplay("0.");
+   getInput("0.");
   } else {
-    pushToDisplay('.');
+   getInput('.');
   }
 }
 document.getElementById('equal').onclick = function(){
@@ -66,25 +76,43 @@ document.getElementById('equal').onclick = function(){
   equalPressed++;  
 }
 
-function pushToDisplay(value){
+document.getElementById('clear').onclick = function(){
+  clear();
+}
+
+function printToDisplay(output) {
+  var outputLength = output.length;
+  output = parseFloat(output);
+
+  if (outputLength > 14){
+    output = output.toExponential();
+    outputLength = output.length;
+    if (outputLength > 14){
+      output = (1 * output).toExponential(10);
+    }
+  }
+  display.innerHTML = output;
+}
+
+function getInput(value){
   displayValue = displayValue + value;
   if (operation == null){
     fisrtNumber = displayValue;
   } else {
     secondNumber = displayValue;
   }
-  display.innerHTML = 1 * displayValue;
+  printToDisplay(displayValue);
   calculateToRegister();
   console.log("n1 = " + fisrtNumber, "n2 = " + secondNumber, "reg = " + register);
 }
 
 function clear() {
   operation = null;
-  displayValue = "";
-  fisrtNumber = null;
+  displayValue = "0";
+  fisrtNumber = 0;
   secondNumber = null;
   register = null;
-  display.innerHTML = displayValue;
+  printToDisplay(displayValue);
 }
 
 function operationPressed (operationNumber) {
@@ -95,7 +123,11 @@ function operationPressed (operationNumber) {
   secondNumber = null;
   displayValue = "";
   operation = operationNumber;
-  display.innerHTML = (register != null) ? register : 0;
+  if (register != null){
+    printToDisplay(register.toString());
+  } else {
+    printToDisplay("0");
+  }
 }
 
 function equal (){
@@ -104,22 +136,22 @@ function equal (){
     calculateToRegister();
   }
   var finalResult = register;
-  display.innerHTML = finalResult;
+  printToDisplay(finalResult.toString());
   console.log("n1 = " + fisrtNumber, "n2 = " + secondNumber, "reg = " + register);
 }
 
 function calculateToRegister() {
   switch (operation) {
-    case 0:
+    case "+":
       register = parseFloat(fisrtNumber) + parseFloat(secondNumber);
       break;
-    case 1:
-      register = parseFloat(fisrtNumber) - parseFloat(secondNumber);
+    case "-":
+      register = (parseFloat(fisrtNumber) - parseFloat(secondNumber)).toPrecision(15) * 1;
       break;
-    case 2:
+    case "x":
       register = parseFloat(fisrtNumber) * parseFloat(secondNumber);
       break;
-    case 3:
+    case "/":
       register = parseFloat(fisrtNumber) / parseFloat(secondNumber);
       break;  
     default:
